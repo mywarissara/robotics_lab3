188ee8dd70526867eea8f1a3ca5b567c2eabe407
Warissara <59024381+warissara946@users.noreply.github.com>
2020-02-03 15:35:17 +0700

    Update week2.py

--- week2.py
+++ week2.py
@@ -1,5 +1,6 @@
 def validate_pin(pin):
-    if (otp.isdecimal() and (len(otp) == 4 or len(otp) == 6 )):
+    pin = str(pin)
+    if (pin.isdigit() and (len(pin) == 4 or len(pin) == 6 )):
         return True
     return False
-
+    
